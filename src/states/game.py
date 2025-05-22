ef1e5ab4a65c93c7f1f75e499b03af3d007a656a
Icecreambobcat <icekingrex123@gmail.com>
2025-05-22 20:08:39 +1000

    Framework update WIP

--- src/states/game.py
+++ src/states/game.py
@@ -1,10 +1,10 @@
 from __future__ import annotations, with_statement
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 import pygame as pg
 from pygame import Surface, display, sprite, time, transform
 
 from app.conf import Config
-from app.lib import Entity, FS_Daemon, GameState
+from app.lib import Entity, FS_Daemon, GameState, Object
 
 
 class Game(GameState):
@@ -44,30 +44,34 @@ class Game(GameState):
             (),
         )
 
-        # sect: sprites & group handling
-        self.playerssprites = sprite.Group()
-        self.enemiessprites = sprite.Group()
-        self.overlaysprites = sprite.Group()
-
         # TODO: add some for [match name] in fsd[type]... loop to load everything in 1 go
 
-
         # sect: init player
+        # TODO: implement missing methods
+        # Implement textures
+        # implement naming
+
+        # self.player = self.Player()
+        # self.enemy = self.Enemy()
 
     def loop(self) -> bool:
         self.initialise_gamestate()
         gamestate = True
         while gamestate:
             self.render()
+            self.process_behaviour()
             self.clock.tick(int(self.config.config["fps"]))
         return False
 
     def render(self) -> None:
+        # NOTE: This much should all be obvious. The overlay needs more work
         self.screen.fill((0, 0, 0))
+        self.screen.blit(self.bg, (0, 0))
         # TODO: Implement logic control here by checking self.status (dict)
         display.flip()
 
     def process_behaviour(self) -> None:
+        # TODO: Handle player input here + call class logic
         pass
 
     def initialise_gamestate(self) -> None:
@@ -90,9 +94,142 @@ class Game(GameState):
     def screen(self) -> Surface:
         return self._scr
 
-    # TODO: Implement behaviours for these 2 classes
-    class Player(Entity):
-        pass
+    # TODO: Implement behaviours for these 3 classes
+    # add logic
+    # NOTE: Here, renderered sprites are nested into the overall class handling behaviour
+    class Player(Entity.Player):
+        _hp: int
+        _sp: PlayerSprite
+        _cards: List[Entity.Player.Card]
 
-    class Enemy(Entity):
-        pass
+        class PlayerSprite(Object):
+            _pos: Tuple[int, int]
+            _tex: Surface
+
+            def __init__(self, tex) -> None:
+                self._pos = (0, 0)
+                self._tex = tex
+
+            @property
+            def position(self) -> Tuple[int, int]:
+                return self._pos
+
+            @position.setter
+            def position(self, pos: Tuple[int, int]) -> None:
+                self._pos = pos
+
+            @property
+            def tex(self) -> Surface:
+                return self._tex
+
+        class Card(Entity.Player.Card):
+            # TODO: implement the card framework and a few basic cards
+            pass
+
+        def __init__(self, tex: Surface) -> None:
+            self._sp = self.PlayerSprite(tex)
+            self._hp = 500
+            self.maxhp = self._hp
+            # Lets just arbitrarily put 500... will see how this goes
+            # Set the max hp to whatever the character is initialised with
+            # I'm too lazy to do otherwise
+
+        @property
+        def health(self) -> int:
+            return self._hp
+
+        # NOTE: Using these setter methods is a catch-all way to be lazy when expanding
+        @health.setter
+        def health(self, hp: int) -> None:
+            match hp:
+                case num if num < 0:
+                    self.hp = 0
+                case num if num > self.maxhp:
+                    self.hp = self.maxhp
+                case _:
+                    self._hp = hp
+
+        @property
+        def sprite(self) -> PlayerSprite:
+            return self._sp
+
+        @property
+        def cards(self) -> List[Entity.Player.Card]:
+            return self._cards
+
+        # TODO: Implement behaviour here
+        def playerturn(self) -> None:
+            pass
+
+    class Enemy(Entity.Enemy):
+        _hp: int
+        _sp: EnemySprite
+        _attacks: List[Entity.Enemy.Attack]
+
+        class EnemySprite(Object):
+            _pos: Tuple[int, int]
+            _tex: Surface
+
+            def __init__(self, tex) -> None:
+                self._pos = (0, 0)
+                self._tex = tex
+
+            @property
+            def position(self) -> Tuple[int, int]:
+                return self._pos
+
+            @position.setter
+            def position(self, pos: Tuple[int, int]) -> None:
+                self._pos = pos
+
+            @property
+            def tex(self) -> Surface:
+                return self._tex
+
+        class EnemyAttack(Entity.Enemy.Attack):
+            # TODO: add a few basic attacks for enemies too
+            pass
+
+        def __init__(self, tex: Surface, hp: int, name: str) -> None:
+            self._sp = self.EnemySprite(tex)
+            self._hp = hp
+            self._name = name
+            # self._attacks = []
+            self.maxhp = self._hp
+            # let's just do this again
+
+        @property
+        def name(self) -> str:
+            return self._name
+
+        @property
+        def health(self) -> int:
+            return self._hp
+
+        @health.setter
+        def health(self, hp: int) -> None:
+            match hp:
+                case num if num < 0:
+                    self.hp = 0
+                case num if num > self.maxhp:
+                    self.hp = self.maxhp
+                case _:
+                    self._hp = hp
+
+        @property
+        def sprite(self) -> EnemySprite:
+            return self._sp
+
+        @property
+        def attacks(self) -> List[Entity.Enemy.Attack]:
+            return self._attacks
+
+        # TODO: implement enermyturn handling
+        def enemyturn(self) -> None:
+            pass
+
+    class Overlay(Object):
+        def __init__(
+            self, position: Tuple[int, int], size: Tuple[int, int], tex: Surface
+        ) -> None:
+            pass
